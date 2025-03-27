9c19e7fbfc85507415ceb638b96ac70a387ec124
Vigilante-Vilay <151979766+Vigilante-Vilay@users.noreply.github.com>
2025-03-27 07:58:08 +0530

    Added DeathCounter

---
+++ scripts/DeathManager.gd
@@ -0,0 +1,6 @@
+extends Node
+
+var death_count = 0  # Global counter
+
+func add_death():
+	death_count += 1
--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -2,8 +2,12 @@ extends Node
 
 var score = 0
 @onready var score_label: Label = $ScoreLabel
-
+@onready var death_label: Label = $DeathCounter
 
 func addPoint():
 	score +=1
 	score_label.text = "You Collected "+ str(score) +" coins."
+
+# Label to display deaths
+func _process(_delta):
+	death_label.text = "You Died " + str(DeathManager.death_count) + " times."
